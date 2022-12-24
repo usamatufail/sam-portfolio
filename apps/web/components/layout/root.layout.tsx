da1473f122072ebc79d7a32048119cac3cdbc9bc
Usama Tufail <miansamusama@gmail.com>
2022-12-24 09:24:12 +0500

    feat(apps/web): added framer-motion and implemented animations

--- apps/web/components/layout/root.layout.tsx
+++ apps/web/components/layout/root.layout.tsx
@@ -1,5 +1,6 @@
 import { Footer, Header } from '..';
 import Head from 'next/head';
+import { motion } from 'framer-motion';
 
 export function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,7 +11,20 @@ export function RootLayout({ children }: { children: React.ReactNode }) {
       </Head>
       <main>
         <Header />
-        <div className="min-h-[calc(100vh_-_128px)]">{children}</div>
+        <div className="min-h-[calc(100vh_-_128px)] overflow-hidden">
+          <motion.div
+            initial={{ x: 300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: 300, opacity: 0 }}
+            transition={{
+              type: 'spring',
+              stiffness: 260,
+              damping: 20,
+            }}
+          >
+            {children}
+          </motion.div>
+        </div>
         <Footer />
       </main>
     </div>
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app';
 import 'styles/globals.css';
+import { AnimatePresence } from 'framer-motion';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AnimatePresence mode="wait" initial={true}>
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
 }
