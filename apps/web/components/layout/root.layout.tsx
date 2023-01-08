9f325e67f5f0b647d89d260943e31047b4a53e20
Usama <miansamusama@gmail.com>
2023-01-08 18:02:35 +0500

    feat(root-layout): added next-seo in the root layout

--- apps/web/components/layout/root.layout.tsx
+++ apps/web/components/layout/root.layout.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
 import { Animated, Footer, Header } from '..';
 
 export function RootLayout({ children }: { children: React.ReactNode }) {
@@ -8,9 +9,18 @@ export function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Head>
-        <title>Sam&apos;s Portfolio App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <NextSeo
+        title="Sam's Portfolio App"
+        description="Sam is a full-stack web and mobile application
+                developer with more than 4 years of work experience in this
+                field. As a programmer, Sam is a skilled and knowledgeable
+                individual with a passion for creating and improving softwares
+                and solving problems. Sam is highly analytical, able to break
+                down complex problems into manageable pieces and devise
+                efficient solutions."
+      />
       <main>
         <Header />
         <div className="min-h-[calc(100vh_-_130px)] overflow-hidden">
